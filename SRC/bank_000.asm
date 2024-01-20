05791a3d5c2da6c9665f832aaeec1e9c9e424614
ConConner <egal3d@gmail.com>
2024-01-20 01:35:39 +0100

    four-way-intersection
    Added the ASM for a custom 4 way intersection on a single screen. Heavily based on the conditional Metroid Transition

--- SRC/bank_000.asm
+++ SRC/bank_000.asm
@@ -6324,7 +6324,7 @@ executeDoorScript: ;{ 00:239C
 
     .doorToken_item:
     cp $d0 ; ITEM {
-    jp nz, .nextToken
+    jp nz, .doorToken_intersection
         ; Load item graphics
         ; Set source bank
         ld a, BANK(gfx_items)
@@ -6455,6 +6455,46 @@ executeDoorScript: ;{ 00:239C
             call beginGraphicsTransfer
         pop hl ;}
     jr .nextToken ;}
+
+    .doorToken_intersection:
+    cp $E0 ; {
+    jp nz, .nextToken
+        ; Compare the current scrolling direction
+        ; 1 Right, 2 Left, 4 Up, 8 Down
+        ld a, [doorScrollDirection]
+        cp $1
+        jr nz, .checkScrollLeft ;check if scrolling right
+            jr .loadNewTransition
+        .checkScrollLeft
+        inc hl
+        inc hl
+        cp $2
+        jr nz, .checkScrollUp ;check if scrolling left
+            jr .loadNewTransition
+        .checkScrollUp: ;check if scrolling up
+        inc hl
+        inc hl
+        cp $4
+        jr nz, .executeScrollDown
+            jr .loadNewTransition
+        .executeScrollDown: ;at this point the scrolling has to be down
+        inc hl
+        inc hl
+        inc hl
+            ; to save on space, we will just continue the standard transition
+            jr .nextToken
+    ;}
+
+; Subroutine to load new Transition from a Transition index in the next two bytes at HL
+.loadNewTransition:
+    inc HL
+    ; Load door index
+    ld a, [hl+]
+    ld [doorIndexLow], a
+    ld a, [hl]
+    ld [doorIndexHigh], a
+    ; Execute it
+    jp executeDoorScript
 
 .nextToken:
     ; Wait a frame before reading another token
